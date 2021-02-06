918ac2ba1b20eed2866792ad03aa82537d17f1cc
dustytrash <Lionelbergen@live.com>
2021-02-06 16:51:11 -0600

    added googl analytifcs to heroku site

--- total-war-app/src/heroku-assets/index.php
+++ total-war-app/src/heroku-assets/index.php
@@ -1,1 +1,11 @@
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5VEXS1DWRD"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5VEXS1DWRD');
+</script>
+
 <?php include_once("index.html"); ?>
